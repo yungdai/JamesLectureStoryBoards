e5d63ed7ac4c3fec3def22f884749fd255d16cae
Yung Dai <yungchidai@gmail.com>
2015-06-07 11:27:16 -0400

    commit

--- CardStackView.swift
+++ CardStackView.swift
@@ -13,31 +13,52 @@ class CardStackView: UIView {
     var cardOne: UIView?
     var cardTwo: UIView?
     var cardThree: UIView?
-    var contentView:UIView?
+    var contentView: UIView?
+    
+    let initialRoationAngle = CGFloat(M_PI/180)
     
     override func awakeFromNib() {
         createMySubViews()
-        styleMySubViews()
+        styleAndAddMySubViews()
     }
     
     func createMySubViews() {
         cardOne = UIView(frame: bounds)
         cardTwo = UIView(frame: bounds)
         cardThree = UIView(frame: bounds)
+        contentView = UIView(frame: bounds)
+        
+
+        
+
     }
     
-    func styleMySubViews {
-        styleMyView(cardOne!)
-        styleMyView(cardTwo!)
-        styleMyView(cardThree!)
+    func styleAndAddMySubViews() {
+        if let cOne = cardOne, cTwo = cardTwo, cThree = cardThree, content = contentView {
+            styleMyView(cOne)
+            cOne.transform = CGAffineTransformMakeRotation(initialRoationAngle)
+            addSubview(cOne)
+            
+            styleMyView(cTwo)
+            cTwo.transform = CGAffineTransformMakeRotation(-initialRoationAngle)
+            addSubview(cTwo)
+            
+            styleMyView(cThree)
+            cThree.transform = CGAffineTransformMakeRotation(2 * initialRoationAngle)
+            addSubview(cThree)
+            
+            styleMyView(content)
+            addSubview(content)
+        }
     }
     
     func styleMyView(view: UIView) {
         view.backgroundColor = UIColor.lightGrayColor()
         view.layer.borderWidth = 1.0
-        view.layer.cornerRadius = bounds.size.height / 2
+        view.layer.borderColor = UIColor.blackColor().CGColor
+        view.layer.cornerRadius = 5
+        transform = CGAffineTransformMakeRotation(initialRoationAngle)
     }
-    
     
 
 }
